1da71cd924f97aae402ea41cd36556c0e7dbc5b3
Pasha Otverchenko <pasha.otverchenko@gmail.com>
2023-05-07 17:51:20 -0700

    Initial setup

---
+++ TestSparrow/ThirdTaskVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TestSparrow
+//
+//  Created by Pasha Otverchenko on 05.05.2023.
+//
+
+import UIKit
+
+final class ThirdTaskVC: UIViewController {
+    
+    // MARK: - UI Elements -
+    
+    private lazy var squareView = UIView()
+    private lazy var switchView = UISlider()
+    
+    // MARK: - Lifecycle -
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    // MARK: - Objective-C Methods -
+    
+    
+    // MARK: - UI Configuration -
+    
+}
+
