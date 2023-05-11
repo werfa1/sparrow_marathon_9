ca45d3ca278d77e16fc78154711a6fa3ffc887c8
Pasha Otverchenko <pasha.otverchenko@gmail.com>
2023-05-11 09:49:59 -0700

    Initial setup

---
+++ TestSparrow/FourthTaskVC.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  TestSparrow
+//
+//  Created by Pasha Otverchenko on 05.05.2023.
+//
+
+import UIKit
+
+final class FourthTaskVC: UIViewController {
+    
+    // MARK: - UI Elements -
+    
+    private lazy var tableView = UITableView()
+    
+    // MARK: - Fields -
+    
+    
+    // MARK: - Lifecycle -
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Task 4"
+    }
+    
+    // MARK: - Objective-C Methods -
+    
+    // MARK: - Animation -
+    
+    
+    // MARK: - UI Configuration -
+    
+}
+
