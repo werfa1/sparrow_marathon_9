1b7f64a1b45f1ddafbe0eb9ce2c274067d11c039
Pasha Otverchenko <pasha.otverchenko@gmail.com>
2023-05-11 11:01:05 -0700

    Added moving on top animation

--- TestSparrow/FourthTaskVC.swift
+++ TestSparrow/FourthTaskVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class FourthTaskVC: UIViewController {
+final class FourthTaskVC: UIViewController, UITableViewDelegate {
     
     // MARK: - UI Elements -
     
@@ -15,21 +15,66 @@ final class FourthTaskVC: UIViewController {
     
     // MARK: - Fields -
     
+    private let cellIdentifier = "cell"
+    private var diffableDataSource: UITableViewDiffableDataSource<Int, Int>!
+    private var snapshot: NSDiffableDataSourceSnapshot<Int, Int>!
+    
+    private var data = Array(0...40)
     
     // MARK: - Lifecycle -
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        configureTableView()
         title = "Task 4"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "shuffle"),
+            style: .done,
+            target: self,
+            action: #selector(handleShuffling))
     }
     
     // MARK: - Objective-C Methods -
     
+    @objc
+    private func handleShuffling() {
+        print("shuffle")
+    }
+    
     // MARK: - Animation -
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = data[indexPath.row]
+        data.remove(at: indexPath.row)
+        data.insert(item, at: 0)
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Int>()
+        snapshot.appendSections([1])
+        snapshot.appendItems(data)
+        
+        diffableDataSource.apply(snapshot, animatingDifferences: true)
+    }
     
     // MARK: - UI Configuration -
     
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        
+        diffableDataSource = UITableViewDiffableDataSource<Int, Int>(tableView: tableView, cellProvider: { tableView, indexPath, itemIdentifier in
+            let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+            cell.textLabel?.text = "\(itemIdentifier)"
+            return cell
+        })
+        diffableDataSource.defaultRowAnimation = .bottom
+        snapshot = NSDiffableDataSourceSnapshot<Int, Int>()
+        snapshot.appendSections([1])
+        snapshot.appendItems(data)
+        diffableDataSource.apply(snapshot)
+    }
 }
 
