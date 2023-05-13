bcc0f5c43c0c264058408b0e809637745d132903
Pasha Otverchenko <pasha.otverchenko@gmail.com>
2023-05-13 14:07:05 -0700

    Task 5 finished

--- TestSparrow/FifthTaskVC.swift
+++ TestSparrow/FifthTaskVC.swift
@@ -7,9 +7,11 @@
 
 import UIKit
 
-final class FifthTaskVC: UIViewController {
+final class FifthTaskVC: UIViewController, UIPopoverPresentationControllerDelegate {
     
     // MARK: - UI Elements -
+    
+    private var button: UIButton!
         
     // MARK: - Fields -
         
@@ -18,16 +20,51 @@ final class FifthTaskVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        configureButton()
     }
     
     // MARK: - Objective-C Methods -
     
-    // MARK: - Helpers -
+    @objc
+    private func handlePresentingVC() {
+        let vc = CompactVC()
+        vc.preferredContentSize = CGSize(width: 300, height: 280)
+        vc.modalPresentationStyle = .popover
+        if let pres = vc.presentationController {
+            pres.delegate = self
+        }
+        present(vc, animated: true)
+        
+        if let pop = vc.popoverPresentationController {
+            pop.delegate = self
+            pop.sourceView = button
+            pop.sourceRect = button.bounds
+            pop.permittedArrowDirections = .up
+        }
+    }
     
-    // MARK: - TableView Delegate -
+    // MARK: - Popover Delegate -
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
     
     // MARK: - UI Configuration -
     
-    
+    private func configureButton() {
+        var config = UIButton.Configuration.gray()
+        config.baseForegroundColor = .systemPink
+        config.title = "Present"
+        
+        button = UIButton(configuration: config)
+        button.addTarget(self, action: #selector(handlePresentingVC), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: view.topAnchor, constant: 150)
+        ])
+    }
 }
 
