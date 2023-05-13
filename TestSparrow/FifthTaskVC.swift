4772648ea2c403ecf42271d60f032c198a555001
Pasha Otverchenko <pasha.otverchenko@gmail.com>
2023-05-13 11:49:06 -0700

    Initial setup

---
+++ TestSparrow/FifthTaskVC.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TestSparrow
+//
+//  Created by Pasha Otverchenko on 05.05.2023.
+//
+
+import UIKit
+
+final class FifthTaskVC: UIViewController {
+    
+    // MARK: - UI Elements -
+        
+    // MARK: - Fields -
+        
+    // MARK: - Lifecycle -
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    // MARK: - Objective-C Methods -
+    
+    // MARK: - Helpers -
+    
+    // MARK: - TableView Delegate -
+    
+    // MARK: - UI Configuration -
+    
+    
+}
+
