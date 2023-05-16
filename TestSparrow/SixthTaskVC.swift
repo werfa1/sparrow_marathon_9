2f3481656a639c24fa16d26fd7d0980d5025a555
Pasha Otverchenko <pasha.otverchenko@gmail.com>
2023-05-16 10:34:41 -0700

    Initial setup

---
+++ TestSparrow/SixthTaskVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TestSparrow
+//
+//  Created by Pasha Otverchenko on 05.05.2023.
+//
+
+import UIKit
+
+final class SixthTaskVC: UIViewController {
+    
+    // MARK: - UI Elements -
+        
+    // MARK: - Fields -
+        
+    // MARK: - Lifecycle -
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+    }
+    
+    // MARK: - Objective-C Methods -
+    
+    // MARK: - Popover Delegate -
+    
+    // MARK: - UI Configuration -
+}
+
